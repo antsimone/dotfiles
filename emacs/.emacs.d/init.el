b2345096d66d218bf3a1d3a5768282ab827acdc9
Antonio Simone <antsimone03@gmail.com>
2020-10-14 12:47:06 +0200

    init vhdl-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,20 +9,26 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+
 (set-frame-font "monospace:pixelsize=12")
 (global-hl-line-mode t) 
 (global-display-line-numbers-mode) 
-
 (setq display-line-numbers-type 'relative) 
+(setq-default indent-tabs-mode nil)
+
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq vc-follow-symblink t)
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
-;; (helm-mode t)
-;;(load-theme 'base16-black-metal-bathory) 
-(load-theme 'modus-vivendi)
-(setq-default indent-tabs-mode nil)
 
+;; vhdl-mode
+(setq user-full-name "Antonio Simone")
+(setq vhdl-basic-offset 4)
+(setq vhdl-standard '(93 nil))
+(setq vhdl-upper-case-attributes t)
+(setq vhdl-upper-case-enum-values t)
+
+(load-theme 'modus-vivendi)
 
