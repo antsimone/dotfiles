fe15168a9618f3271914377894e64b520712b2c3
Antonio Simone <antsimone03@gmail.com>
2020-10-14 18:20:28 +0200

    clean init-evil, redundant evil-window-map

--- emacs/.emacs.d/init-evil.el
+++ emacs/.emacs.d/init-evil.el
@@ -17,10 +17,3 @@
 
 (powerline-evil-vim-theme)
 (powerline-evil-vim-color-theme)
-
-(eval-after-load "evil"
-  '(progn
-     (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-     (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-     (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-     (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)))
